14b13f7503dcd8a3ad4a6dd825b1b84cda23183c
Derek Finlinson <dcfinlinson@gmail.com>
2020-08-25 08:10:38 -0600

    Return empty string if null

--- src/models.ts
+++ src/models.ts
@@ -1,43 +1,43 @@
 export class WebApiConfig {
-    public version: string;
-    public accessToken?: string;
-    public url?: string;
+  public version: string;
+  public accessToken?: string;
+  public url?: string;
 
-    /**
-     * Constructor
-     * @param config WebApiConfig
-     */
-    constructor(version: string,  accessToken?: string, url?: string) {
-        // If URL not provided, get it from Xrm.Context
-        if (url == null) {
-            const context: Xrm.GlobalContext =
-                typeof GetGlobalContext !== 'undefined' ? GetGlobalContext() : Xrm.Utility.getGlobalContext();
-            url = `${context.getClientUrl()}/api/data/v${version}`;
+  /**
+   * Constructor
+   * @param config WebApiConfig
+   */
+  constructor(version: string, accessToken?: string, url?: string) {
+    // If URL not provided, get it from Xrm.Context
+    if (url == null) {
+      const context: Xrm.GlobalContext =
+        typeof GetGlobalContext !== 'undefined' ? GetGlobalContext() : Xrm.Utility.getGlobalContext();
+      url = `${context.getClientUrl()}/api/data/v${version}`;
 
-            this.url = url;
-        } else {
-            this.url = `${url}/api/data/v${version}`;
-            this.url = this.url.replace('//', '/');
-        }
-
-        this.version = version;
-        this.accessToken = accessToken;
+      this.url = url;
+    } else {
+      this.url = `${url}/api/data/v${version}`;
+      this.url = this.url.replace('//', '/');
     }
+
+    this.version = version;
+    this.accessToken = accessToken;
+  }
 }
 
 export interface WebApiRequestResult {
-    error: boolean;
-    response: any;
-    headers?: any;
+  error: boolean;
+  response: any;
+  headers?: any;
 }
 
 export interface WebApiRequestConfig {
-    method: string;
-    contentType: string;
-    body?: any;
-    queryString: string;
-    apiConfig: WebApiConfig;
-    queryOptions?: QueryOptions;
+  method: string;
+  contentType: string;
+  body?: any;
+  queryString: string;
+  apiConfig: WebApiConfig;
+  queryOptions?: QueryOptions;
 }
 
 /**
@@ -45,13 +45,17 @@ export interface WebApiRequestConfig {
  * @param id GUID to parse
  */
 export function parseGuid(id: string): string {
-    id = id.replace(/[{}]/g, '');
+  if (id === null || id === 'undefined') {
+    return '';
+  }
 
-    if (/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(id)) {
-        return id.toUpperCase();
-    } else {
-        throw Error(`Id ${id} is not a valid GUID`);
-    }
+  id = id.replace(/[{}]/g, '');
+
+  if (/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(id)) {
+    return id.toUpperCase();
+  } else {
+    throw Error(`Id ${id} is not a valid GUID`);
+  }
 }
 
 /**
@@ -60,41 +64,41 @@ export function parseGuid(id: string): string {
  * @param id2 GUID 2
  */
 export function areGuidsEqual(id1: string, id2: string): boolean {
-    id1 = parseGuid(id1);
-    id2 = parseGuid(id2);
+  id1 = parseGuid(id1);
+  id2 = parseGuid(id2);
 
-    if (id1 === null || id2 === null || id1 === undefined || id2 === undefined) {
-        return false;
-    }
+  if (id1 === null || id2 === null || id1 === undefined || id2 === undefined) {
+    return false;
+  }
 
-    return id1.toLowerCase() === id2.toLowerCase();
+  return id1.toLowerCase() === id2.toLowerCase();
 }
 
 export interface QueryOptions {
-    includeFormattedValues?: boolean;
-    includeLookupLogicalNames?: boolean;
-    includeAssociatedNavigationProperties?: boolean;
-    maxPageSize?: number;
-    impersonateUserId?: string;
-    representation?: boolean;
+  includeFormattedValues?: boolean;
+  includeLookupLogicalNames?: boolean;
+  includeAssociatedNavigationProperties?: boolean;
+  maxPageSize?: number;
+  impersonateUserId?: string;
+  representation?: boolean;
 }
 
 export interface Entity {
-    [propName: string]: any;
+  [propName: string]: any;
 }
 
 export interface RetrieveMultipleResponse {
-    value: Entity[];
-    '@odata.nextlink': string;
+  value: Entity[];
+  '@odata.nextlink': string;
 }
 export interface ChangeSet {
-    queryString: string;
-    entity: object;
-    method: string;
+  queryString: string;
+  entity: object;
+  method: string;
 }
 
 export interface FunctionInput {
-    name: string;
-    value: string;
-    alias?: string;
+  name: string;
+  value: string;
+  alias?: string;
 }
